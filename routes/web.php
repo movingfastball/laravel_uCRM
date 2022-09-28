e991c1430f443b2a0758a098f2152dd487d188cd
movingfastball <movingfastballny2012@yahoo.co.jp>
2022-09-28 18:15:48 +0900

    旅行前の変更差分

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-
+use Inertia\Inertia;
+use App\Http\Controllers\InertiaTestController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/inertia-test', function() {
+        return Inertia::render('InertiaTest');
+    }
+);
+Route::get('/inertia/index', [InertiaTestController::class, 'index'])->name('inertia.index');
+Route::get('/inertia/show/{id}', [InertiaTestController::class, 'show'])->name('inertia.show');
+Route::post('/inertia', [InertiaTestController::class, 'store'])->name('inertia.store');
+
+
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
