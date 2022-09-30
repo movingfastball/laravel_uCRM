d7f377fc14037df6a2749cc2a3a14a9444e88a42
movingfastball <movingfastballny2012@yahoo.co.jp>
2022-09-30 16:19:55 +0900

    itemの下準備

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\InertiaTestController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\ItemController;
 
 Route::get('/inertia-test', function() {
         return Inertia::render('InertiaTest');
@@ -28,6 +19,7 @@ Route::get('/component-test',function(){
     return Inertia::render('ComponentTest');
 });
 
+Route::resource('items', ItemController::class)->middleware(['auth', 'verified']);
 
 
 Route::get('/', function () {
